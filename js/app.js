6ccab746511e64ddaee6171f88b899ed705d13fd
RajeshNPCI <p.rajesh531@gmail.com>
2017-01-17 00:15:59 +0530

    FEWDFS5A4
    FEWDFS5A4

---
+++ js/app.js
@@ -0,0 +1,59 @@
+var employeeArray=[]
+
+var employee1={
+	name:"Rajesh",
+	age:26,
+	salary:70000,
+	address:{
+		city:"kollapur",
+		state:"TelanganaState",
+		pincode:509102,
+	}
+};
+
+var employee2={
+	name:"Santhosh",
+	age:25,
+	salary:60000,
+	address:{
+		city:"Vizag",
+		state:"Andhra Pradesh",
+		pincode:500001,
+	}
+};
+
+var employee3={
+	name:"Naresh",
+	age:23,
+	salary:50000,
+	address:{
+		city:"Hyderabad",
+		state:"TelanganaState",
+		pincode:500081,
+	}
+};
+
+var employee4={
+	name:"Munesh",
+	age:26,
+	salary:70000,
+	address:{
+		city:"Hyderabad",
+		state:"TelanganaState",
+		pincode:500034,
+	}
+};
+
+var employee5={
+	name:"Shravan",
+	age:29,
+	salary:90000,
+	address:{
+		city:"Secunderabad",
+		state:"TelanganaState",
+		pincode:500023,
+	}
+};
+
+employeeArray[0]=employee1;
+console.log(employeeArray[0]);
